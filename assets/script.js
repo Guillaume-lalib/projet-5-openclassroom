4a86d1e29f0c1f9285392e2db8081469e0964abf
Laliberté Guillaume <guillaume.lalib@gmail.Com>
2022-12-23 14:44:12 +0100

    slide right ok slide left in progress

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,22 @@
 const slides = [
 	{
-		"image":"slide1.jpg",
+		"image":"./assets/images/slideshow/slide1.jpg",
 		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
 	},
 	{
-		"image":"slide2.jpg",
+		"image":"./assets/images/slideshow/slide2.jpg",
 		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
 	},
 	{
-		"image":"slide3.jpg",
+		"image":"./assets/images/slideshow/slide3.jpg",
 		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
 	},
 	{
-		"image":"slide4.png",
+		"image":"./assets/images/slideshow/slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
 const left = document.getElementById("left");
 const right = document.getElementById("right");
 const img = document.querySelector("#banner > img")
@@ -23,39 +24,39 @@ const text = document.querySelector("#banner > p")
 console.log(img);
 console.log(text);
 
-// console.log(imgSrc);
-// console.log(textSrc);
-
-
-left.addEventListener('click', function() {
-	for (let i = 0; i < numbers; i++) {
-		text.innerHTML = slides[i].tagLine
-}
-	// img.innerHTML = "./assets/images/slideshow/slide3.jpg"
-	console.log("okleft")
-})
-
+var imageIndex = 1
+var textIndex = 1
 
 right.addEventListener('click', function() {
-		for (let i = 0; i < numbers; i++) {
-		text.innerHTML = slides[i].tagLine
-		i++
-}
-	console.log("okright")
+	img.setAttribute("src", slides[imageIndex].image)
+	imageIndex++;
+	if (imageIndex > 3) {imageIndex = 0}
+
+	text.innerHTML = slides[textIndex].tagLine
+	textIndex++;
+	if (textIndex > 3) {textIndex = 0}
+
+})
+left.addEventListener('click', function() {
+	img.setAttribute("src", slides[imageIndex].image)
+	imageIndex--;
+	if (imageIndex > -1) {imageIndex = 4}
+
+	text.innerHTML = slides[textIndex].tagLine
+	textIndex--;
+	if (textIndex > -1) {textIndex = 4}
+	console.log(text);
+
 })
 
-
-console.log(addDots);
-
 const bullet = document.querySelector(".dots");
-numbers = slides.length;
 
-for (let i = 0; i < numbers; i++) {
+for (let i = 0; i < slides.length; i++) {
 	div=document.createElement("div")
 	div.className="dot"
 	bullet.appendChild(div)
 }
 
-console.log(slides);
+console.log(addDots);
 
 
